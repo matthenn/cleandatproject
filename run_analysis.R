303b993a2ecf42f7c34971afba53de8dc3da8227
matthenn <mhenn@alumni.ubc.ca>
2015-03-22 11:38:25 -0700

    final commit

--- run_analysis.R
+++ run_analysis.R
@@ -8,62 +8,46 @@ library(data.table)
 
 address <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 address <- sub("^https", "http", address)
-td <- dir.create("td")
+if(!file.exists("tempdir")) {
+  dir.create("tempdir") }
+download.file(address, destfile = "./tempdir/datfile.zip",  method = "curl")
+dat <- unzip("./tempdir/datfile.zip", exdir = "./tempdir")
 
-dat <- download.file(address, destfile = "./td",  method = "curl")
+# get the data of interest to this assignment
+test <- list.files("./tempdir/UCI HAR Dataset/test",
+                   pattern = "*.txt", 
+                   full.names = TRUE)
+testdat <- lapply(test, read.table)
 
-list.files(dat)
-
-
-
-
-datfile <- "/Users/Matt/current classes/coursera dssp/cleaning data/project/getdata_projectfiles_UCI HAR Dataset.zip"
-dat <- unzip(datfile)
-
-# read in the test and train data files
-setwd("/Users/Matt/current classes/coursera dssp/cleaning data/project/UCI HAR Dataset/test")
-
-testdat <- list.files(path = 
-         "/Users/Matt/current classes/coursera dssp/cleaning data/project/UCI HAR Dataset/test")
-
-testdata <- lapply(testdat[2:4], read.table)
-
-alltestdat <- cbind.data.frame(subject_test = testdata[1], test_label = testdata[3], testdata[2])
-                               
+train <- list.files("./tempdir/UCI HAR Dataset/train",
+                    pattern = "*.txt",
+                    full.names = TRUE)
+traindat <- lapply(train, read.table)
 
 
-setwd("/Users/Matt/current classes/coursera dssp/cleaning data/project/UCI HAR Dataset/train")
+# start combining the data
 
-traindat <- list.files(path = 
-      "/Users/Matt/current classes/coursera dssp/cleaning data/project/UCI HAR Dataset/train")
-
-traindata <- sapply(traindat[2:4], read.table)
-
-alltraindata <- cbind.data.frame(traindata[1], traindata[3], traindata[2])
+alltestdat <- cbind.data.frame(subject_test = testdat[1], test_label = testdat[3], testdat[2])                               
+alltraindata <- cbind.data.frame(traindat[1], traindat[3], traindat[2])
 
 ldat <- list(alltestdat, alltraindata)
             
 alldat <- as.data.frame(rbindlist(ldat, use.names = FALSE, fill = FALSE))
 
 # read in features.txt to get list of features to use as colnames
-
 label <- read.table(
-  "/Users/Matt/current classes/coursera dssp/cleaning data/project/UCI HAR Dataset/features.txt", 
+  "./tempdir/UCI HAR Dataset/features.txt", 
   colClasses = "character")
 
 labels <- rbind("subject_test", "test_label", label)
-
 names(alldat) <- c(labels$V2)
-
 labs <- grep("mean|std", labels$V2, value = TRUE)
 
 # extract only measurements that are measuring a mean or stdev. 
 alldat1 <- cbind.data.frame(alldat[1], alldat[2], alldat[grep("mean|std", labels$V2, value = TRUE)])
 
 
-
 # replace test_label with activity name
-# yep, it's ghetto but it worked. easier way to do this?
 actdat <- alldat1$test_label
 
 a1 <- replace(actdat, actdat == 1, "WALKING")
@@ -84,7 +68,8 @@ fd <- as.data.frame(aggregate(alldat1, list(alldat1[,1], alldat1[,2]), mean))
 #clean it up a bit
 fd$test_label <- fd$Group.2
 fd <- fd[,3:83]
-write.table(fd, "tidydat", row.name = FALSE)
+
+write.table(fd, "tidydat.txt", row.name = FALSE)
 
 # done. fd is a tidy dataset. 
 
